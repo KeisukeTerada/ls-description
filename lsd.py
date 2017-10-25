7c0a2ec5ac5add4e7ad9ee7cfc99315fbf44d58a
Keisuke Terada <terada.k.ac@m.titech.ac.jp>
2017-10-25 17:10:15 +0900

    lsd.pyを作成

---
+++ lsd.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pyyaml
+import argparse
+
+FILENAME = "./.lsd.yaml"
+
+"""help
+-l look
+-c add comment, added time
+-f filename
+-i interpreter mode
+-h show this message
+"""
+
